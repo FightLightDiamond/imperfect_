4cbd00116b948a914e308bd172ac14de92aeb5c3
Admin <adminadmin@Phams-MacBook-Pro.local>
2020-07-30 21:20:39 +0700

    1

---
+++ src/stores/redux/file-lesson/actions.js
@@ -0,0 +1,21 @@
+import {
+  LOGIN_USER,
+  LOGIN_USER_SUCCESS,
+
+  LOGIN_USER_ERROR,
+} from '../actions';
+
+export const loginUser = (user, history) => ({
+  type: LOGIN_USER,
+  payload: { user, history }
+});
+
+export const loginUserSuccess = (user) => ({
+  type: LOGIN_USER_SUCCESS,
+  payload: user
+});
+
+export const loginUserError = (message) => ({
+  type: LOGIN_USER_ERROR,
+  payload: { message }
+});
